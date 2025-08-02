ec2075f63ed3ea52ba6c14b72e84bb5e6f99f282
cagan7 <caganaep7@gmail.com>
2025-08-02 23:51:50 +0300

    Add files via upload

--- players.js
+++ players.js
@@ -1,22 +1,22 @@
 const players = [
   // GALATASARAY
-  { name: "Victor Osimhen", age: 26, position: "ST", team: "Galatasaray", market_value: "€75M" },
-  { name: "Leroy Sané", age: 29, position: "RW", team: "Galatasaray", market_value: "Free" },
-  { name: "Roland Sallai", age: 27, position: "RW", team: "Galatasaray", market_value: "€6M" },
-  { name: "Davinson Sánchez", age: 29, position: "CB", team: "Galatasaray", market_value: "€9.5M" },
-  { name: "Kazımcan Karataş", age: 22, position: "LB", team: "Galatasaray", market_value: "€1.15M" },
+  { name: "Victor Osimhen", age: 26, position: "ST", team: "Galatasaray", market_value: "€70M" },
+  { name: "Leroy Sané", age: 29, position: "RW", team: "Galatasaray", market_value: "€32M" },
+  { name: "Yunus Akgün", age: 27, position: "RW", team: "Galatasaray", market_value: "€14M" },
+  { name: "Davinson Sánchez", age: 29, position: "CB", team: "Galatasaray", market_value: "€20M" },
+  { name: "Barış Alper Yılmaz", age: 22, position: "LB", team: "Galatasaray", market_value: "€21M" },
 
   // FENERBAHÇE
-  { name: "Edin Džeko", age: 39, position: "ST", team: "Fenerbahçe", market_value: "€1.5M" },
+  { name: "Jhon Duran", age: 23, position: "ST", team: "Fenerbahçe", market_value: "€5M" },
   { name: "Fred", age: 32, position: "CM", team: "Fenerbahçe", market_value: "€10M" },
-  { name: "Dusan Tadić", age: 36, position: "LW", team: "Fenerbahçe", market_value: "€3M" },
-  { name: "Cengiz Ünder", age: 28, position: "RW", team: "Fenerbahçe", market_value: "€12M" },
-  { name: "Dominik Livaković", age: 30, position: "GK", team: "Fenerbahçe", market_value: "€8M" },
+  { name: "Archie Brown", age: 22, position: "CM", team: "Fenerbahçe", market_value: "€7M" },
+  { name: "Nelson Semedo", age: 31, position: "RB", team: "Fenerbahçe", market_value: "€7M" },
+  { name: "Dominik Livaković", age: 30, position: "GK", team: "Fenerbahçe", market_value: "€9M" },
 
   // BEŞİKTAŞ
   { name: "Semih Kılıçsoy", age: 20, position: "ST", team: "Beşiktaş", market_value: "€12M" },
-  { name: "Gedson Fernandes", age: 27, position: "CM", team: "Beşiktaş", market_value: "€9M" },
-  { name: "Cenk Tosun", age: 34, position: "ST", team: "Beşiktaş", market_value: "€1.5M" },
-  { name: "Omar Colley", age: 33, position: "CB", team: "Beşiktaş", market_value: "€2.5M" },
-  { name: "Mert Günok", age: 36, position: "GK", team: "Beşiktaş", market_value: "€700K" }
+  { name: "Rafa Silva", age: 29, position: "RW", team: "Beşiktaş", market_value: "€10M" },
+  { name: "Orkun Kökçü", age: 26, position: "CM", team: "Beşiktaş", market_value: "€30M" },
+  { name: "Tammy Abraham", age: 25, position: "ST", team: "Beşiktaş", market_value: "€15M" },
+  { name: "Mert Günok", age: 36, position: "GK", team: "Beşiktaş", market_value: "€1M" }
 ];
